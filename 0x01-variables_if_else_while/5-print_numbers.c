9d33fca5b29b505fc65cc4738bd4e966d811fe39
adam <adam@.gmail.com>
2023-03-18 18:11:29 -0700

    kdkejj

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* include header */
+
+/**
+  * main - print numbers
+  *
+  * Return: Always 0
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
