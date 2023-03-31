dd7887d5bdbe47f063c879760ccc00e01e103017
adam <adam@.gmail.com>
2023-03-30 19:36:44 -0700

    lddlkckc

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+  * _strcmp - strcmp
+  * @s1: s one
+  * @s2: s two
+  * Return: pointer
+  */
+
+int _strcmp(char *s1, char *s2)
+{
+	int p = strcmp(s1, s2);
+
+	return (p);
+}
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *string_toupper - check the code
+ * @s: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+
+char *string_toupper(char *s)
+{
+	char *p = strupr(s);
+
+	return (p);
+}
