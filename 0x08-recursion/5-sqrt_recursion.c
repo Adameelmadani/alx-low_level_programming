35f0838cae09e28895a53f7a31aa94b40cf91c1e
adam <adam@.gmail.com>
2023-04-06 17:58:28 -0700

    slls

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,18 +2,37 @@
 #include "main.h"
 
 /**
-  * _sqrt_recursion - sqrt
+  * _sqrti - sqrt
+  * @n: integer
+  * @i: integer
+  * Return: integer
+  */
+
+int _sqrti(int i, int n)
+{
+	int rac = i * i;
+
+	if (rac == n)
+	{
+		return (i);
+	} else if (i >= ((n / 2) + 1))
+	{
+		return (-1);
+	}
+	return (_sqrti(i + 1, n));
+}
+
+/**
+  * _sqrt_recursion - sq
   * @n: integer
   * Return: integer
   */
 
 int _sqrt_recursion(int n)
 {
-	int a = -1;
-
-	if (n >= 0)
+	if (n < 0)
 	{
-	 	
+		return (-1);
 	}
-	return (a);
+	return (_sqrti(1, n));
 }
