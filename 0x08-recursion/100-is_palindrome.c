27495f3ee35335c058b7f64e4e385cbe55e92a2b
adam <adam@.gmail.com>
2023-04-06 19:40:36 -0700

    dldl

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -3,18 +3,36 @@
 #include <string.h>
 
 /**
-  * is_palindrome - function
+  * fuc - function
   * @s: char
+  * @i: integer
+  * @size: size
   * Return: integer
  */
 
+int fuc(int i, char *s, int size)
+{
+	if (*(s + i) == *(s + size - 1 - i))
+	{
+		if (i == (size / 2) - 1)
+		{
+			return (1);
+		}
+		return (fuc(i + 1, s, size));
+	}
+	return (0);
+}
+
+/**
+  * is_palindrome - main
+  * @s: char
+  * Return: integer
+  */
+
 int is_palindrome(char *s)
 {
-	int size = strlen(s) - 1;
 	int i = 0;
+	int size = strlen(s);
 
-	if (size % 2 == 0)
-	{
-		if (*(s + i) == *(s + size - i))
-		{
-
+	return (fuc(i, s, size));
+}
