9f85ed0e57f20b0e8bbc24b1a6f05dcf24475095
adam <adam@.gmail.com>
2023-04-09 07:15:48 -0700

    lld

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - print program name
+  * @argc: count arg
+  * @argv: vector arg
+  * Return: always 0
+  */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
