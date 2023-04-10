741180526f3ae26d33b4e3c4358ead6e8cc181ca
adam <adam@.gmail.com>
2023-04-10 11:08:12 -0700

    ldl

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * print_array - print all elements of array a
+  * @a: array pointer
+  * @n: integer
+  */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+	printf("%d\n", a[n - 1]);
+}
