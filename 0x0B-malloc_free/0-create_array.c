49d881687c609cc66302611299a4e4a29905438a
adam <adam@.gmail.com>
2023-04-12 13:11:39 -0700

    ld

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,6 +18,10 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 	p = malloc(size * sizeof(char));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
 		p[i] = c;
