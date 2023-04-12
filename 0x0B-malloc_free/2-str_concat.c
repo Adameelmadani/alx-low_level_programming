b9b3b545007b0d25ef5351fd2b5ac39874d61d0f
adam <adam@.gmail.com>
2023-04-12 16:20:54 -0700

    ld

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -30,7 +30,7 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 	{
 		i = 0;
-		strcpy(p, "", 1);
+		strncpy(p, "", 1);
 		i++;
 	} else
 	{
@@ -41,7 +41,7 @@ char *str_concat(char *s1, char *s2)
 	}
 	if (s2 == NULL)
 	{
-		strcpy(p + 1, "", 1);
+		strncpy(p + 1, "", 1);
 	} else
 	{
 		for (j = 0; j < size2; j++)
