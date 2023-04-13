7537872b1f36f469205df0a26ea63c7575e94f10
adam <adam@.gmail.com>
2023-04-12 19:45:04 -0700

    ldd

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,42 +3,35 @@
 #include <stdlib.h>
 
 /**
-  * alloc_grid - function that alloc pointer integer
-  * @width: integer
-  * @height: integer
-  * Return: int pointer to pointer
-  */
+ * alloc_grid - it returns a pointer to a 2 d
+ * @width: positive int
+ * @height: positive int
+ * Return: pointer or NULL
+ */
 
 int **alloc_grid(int width, int height)
 {
-	int i;
-	int j;
 	int **p;
+	int i, j;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	p = malloc(sizeof(*p) * height);
+	p = (int **)malloc(sizeof(int *) * height);
 	if (p == NULL)
-	{
-		free(p);
 		return (NULL);
-	}
+
 	for (i = 0; i < height; i++)
 	{
-		p[i] = malloc(sizeof(int) * width);
+		p[i] = (int *)malloc(sizeof(int) * width);
 		if (p[i] == NULL)
 		{
 			for (; i >= 0; i--)
-			{
 				free(p[i]);
-			}
 			free(p);
 			return (NULL);
 		}
 		for (j = 0; j < width; j++)
-			{
-				p[i][j] = 0;
-			}
+			p[i][j] = 0;
 	}
 	return (p);
 }
