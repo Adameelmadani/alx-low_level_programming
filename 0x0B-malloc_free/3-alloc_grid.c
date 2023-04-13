623bd78eb01c41e831b92cd2a33f9793e4b98873
adam <adam@.gmail.com>
2023-04-12 18:30:52 -0700

    dl

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,12 +17,12 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	*p = malloc(sizeof(**p) * s);
+	p = malloc(sizeof(*p) * s);
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < s; i++)
 	{
 		**(p + i) = 0;
 	}
-	return (*p);
+	return (**p);
 }
