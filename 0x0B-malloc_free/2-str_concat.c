63252d81187d63a78ed5b18957f3faeaf4830416
adam <adam@.gmail.com>
2023-04-12 17:20:26 -0700

    lddl

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,19 +12,19 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int i, j;
-	unsigned int size1;
-	unsigned int size2;
+	unsigned int i = 0, j = 0;
+	unsigned int size1 = 0;
+	unsigned int size2 = 0;
 	char *p;
 
 	for (size1 = 0; s1[size1] != '\0' && s1 != NULL; size1++)
 		;
 	for (size2 = 0; s2[size2] != '\0' && s2 != NULL; size2++)
 		;
-	p = malloc(sizeof(char) * (size1 + size2 + 1));
+	p = (char *)malloc(sizeof(char) * (size1 + size2 + 1));
 	if (p == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
 	for (i = 0; i < size1 && s1 != NULL; i++)
 	{
