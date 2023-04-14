e48da51e992386c64515381dbf73d6794294efd7
adam <adam@.gmail.com>
2023-04-13 19:21:52 -0700

    ldld

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,8 +24,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	for (i = 0; i < len1 && s1 != NULL; i++)
 		p[i] = s1[i];
-	for (i = 0; i < len1 + len2 && s2 != NULL; i++)
+	for (i = 0; i < n && s2 != NULL; i++)
 		p[len1 + i] = s2[i];
+	p[i + len1] = '\0';
 	return (p);
-	free(p);
 }
