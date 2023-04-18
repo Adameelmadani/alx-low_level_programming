ba28697bef390e2297039918c6299e04864cb907
adam <adam@.gmail.com>
2023-04-17 19:21:56 -0700

    ldl

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * new_dog - new dog
+  * @name: name
+  * @age: age
+  * @owner: owner
+  */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *p;
+	
+	p = malloc(sizeof(struct dog));
+	if (p == NULL)
+		return (NULL);
+	p->name = name;
+	p->age = age;
+	p->owner = owner;
+	return (p);
+}
