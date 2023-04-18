614dd3f1b82e95e0c784a0542e99fc6a8a831712
adam <adam@.gmail.com>
2023-04-17 19:45:16 -0700

    dldl

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,29 +15,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *p;
 
 	p = malloc(sizeof(struct dog));
-	if (p == NULL)
+	if (p == NULL || p->name == NULL || p->owner == NULL)
 	{
-		free(p->name);
-		free(p->owner);
 		free(p);
 		return (NULL);
 	}
 	p->name = name;
-	if (p->name == NULL)
-	{
-		free(p->name);
-		free(p->owner);
-		free(p);
-		return (NULL);
-	}
 	p->age = age;
 	p->owner = owner;
-	if (p->owner == NULL)
-	{
-		free(p->name);
-		free(p->owner);
-		free(p);
-		return (NULL);
-	}
 	return (p);
 }
