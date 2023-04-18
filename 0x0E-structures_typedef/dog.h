ef1083d66642f05836d9c1c73a20b3d55488ab0d
adam <adam@.gmail.com>
2023-04-17 17:24:12 -0700

    dldl

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,6 +13,20 @@ struct dog
 	char *owner;
 	float age;
 };
+
+/**
+  * struct dog_t - struct another name
+  * @name: name
+  * @owner: owner
+  * @age: age
+  */
+typedef struct
+{
+	char *name;
+	char *owner;
+	float age;
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
