63b83f996d750ecfe7eb51d5a26b9fedb30fa033
adam <adam@.gmail.com>
2023-04-18 17:08:28 -0700

    lddl

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  * main - main function that prints the file's name
+  * Return: 0 if it's save, 1 if it isn't
+  */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
