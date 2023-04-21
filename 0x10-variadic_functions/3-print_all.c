55e6aac23b25d53c974b908d217daf6938bd9253
Adameelmadani <125741857+Adameelmadani@users.noreply.github.com>
2023-04-21 03:00:36 +0000

    Update 3-print_all.c
    diid

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,37 +1,89 @@
 #include "variadic_functions.h"
+#include <stdio.h>
 
 /**
-  * print_all - print all
-  * @format: format of st
-  */
+ * print_all - prints anything.
+ * @format: list of types of arguments passed to the function
+ */
 
 void print_all(const char * const format, ...)
 {
-	int i = 0;
-	int j = 0;
-	va_list list;
-	char *str;
+	va_list pars;
+	char_list printer[] = {
+		{'c', c_print},
+		{'i', i_print},
+		{'f', f_print},
+		{'s', s_print}
+	};
+	int i, j;
 
-	while (fomat[j] != '\0')
-		j++;
-	while (i < j)
+	i = 0;
+	va_start(pars, format);
+	while (format && format[i])
 	{
-		if (format[i] == 's')
+		j = 0;
+		while (printer[j].chars)
 		{
-			str = va_arg(list, char *);
-			if (format[i] == NULL)
-				str = "(nil)";
-			printf("%s", str);
+			if (format[i] == printer[j].chars)
+			{
+				printer[j].f(pars);
+				if (format[i + 1])
+					printf(", ");
+				break;
+			}
+			j++;
 		}
-		if (format[i] == 'i')
-			printf("%d", va_arg(list, int));
-		if (format[i] == 'f')
-			printf("%f", va_arg(list, float));
-		if (format[i] == 'c')
-			printf("%c", va_arg(list, char));
 		i++;
-		if (i < j)
-			printf(", ");
 	}
-	va_end(list);
+	va_end(pars);
+	printf("\n");
+}
+
+/**
+ * c_print - prints c
+ * @pars: va list
+ */
+
+void c_print(va_list pars)
+{
+	printf("%c", va_arg(pars, int));
+}
+
+/**
+ * i_print - prints i
+ * @pars: va list
+ */
+
+void i_print(va_list pars)
+{
+	printf("%i", va_arg(pars, int));
+}
+
+/**
+ * f_print - prints f
+ * @pars: va list
+ */
+
+void f_print(va_list pars)
+{
+	printf("%f", va_arg(pars, double));
+}
+
+/**
+ * s_print - prints s
+ * @pars: va list
+ */
+
+void s_print(va_list pars)
+{
+	char *p;
+
+	p = va_arg(pars, char *);
+	switch (!!p)
+	{
+		case 0:
+			p = "(nil)";
+			break;
+	}
+	printf("%s", p);
 }
