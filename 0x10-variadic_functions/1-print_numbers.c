191bfcadff76c9722559d96551fe948d0c4fb72d
adam <adam@.gmail.com>
2023-04-20 18:24:52 -0700

    dl

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -22,7 +22,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			if (separator != NULL)
 				printf("%s", separator);
 		}
-		printf("%d\n", va_arg(list, int));
+		printf("%d", va_arg(list, int));
 	}
 	va_end(list);
+	printf("\n");
 }
