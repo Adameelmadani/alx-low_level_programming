fa678888fa494c0c353b6c3beb92b8f048c67ce0
adam <adam@.gmail.com>
2023-04-27 19:55:05 -0700

    ldld

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * free_list - free it
+  * @head: head of list
+  */
+
+void free_list(list_t *head)
+{
+	while(head->next)
+	{
+		free(head->next);
+		head->next = head->next->next;
+	}
+}
