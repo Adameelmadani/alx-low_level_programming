c902ca605f8f4456781179bbfab96f6a790c3ed1
adam <adam@.gmail.com>
2023-05-02 13:23:16 -0700

    lddl

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+  * get_nodeint_at_index - get the nth node
+  * @head: head
+  * @index: index
+  * Return: the nth node
+  */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *temp = NULL;
+	listint_t *stemp = NULL;
+
+	if (!head)
+		return (NULL);
+	temp = head;
+	stemp = temp;
+	for (i = 0; temp && i < index + 1; i++)
+	{
+		stemp = temp;
+		temp = temp->next;
+	}
+	if ((i - 1) != index)
+		return (NULL);
+	return (stemp);
+}
