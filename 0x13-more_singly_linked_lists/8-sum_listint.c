e240d715015100cf49cc8b4f18d4d602834e2c1f
adam <adam@.gmail.com>
2023-05-03 04:10:42 -0700

    ldld

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+  * sum_listint - sum of all datas
+  * @head: head
+  * Return: integer
+  */
+int sum_listint(listint_t *head)
+{
+	listint_t *temp = NULL;
+	int sum = 0;
+
+	if (!head)
+		return (0);
+	temp = head;
+	while (temp)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+	return (sum);
+}
