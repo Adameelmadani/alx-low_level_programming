026164be1bef8fe5278bdff6e9a9197e162061b4
adam <adam@.gmail.com>
2023-05-04 19:29:37 -0700

    ldld

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,4 +8,15 @@
   */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned long int j = 0;
+	int l = 0;
+
+	j = n * 0 + 1;
+	while (j != 0)
+	{
+		index++;
+	}
+	if (index == 0)
+		l = 1;
+	return (l);
 }
