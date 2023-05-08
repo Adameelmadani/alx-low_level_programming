2292740cb90dc983f5e7425dce0db1d80f46dfb3
adam <adam@.gmail.com>
2023-05-08 16:41:58 -0700

    dld

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+  * create_file - create a file
+  * @filename: filename
+  * @text_content: text content
+  * Return: int
+  */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (!filename)
+	{
+		write(1, "fail", 4);
+		return (-1);
+	}
+	if (!text_content)
+	{
+		fd = open(filename, O_WRONLY | O_CREAT, 0600);
+		close(fd);
+		return (1);
+	}
+	fd = open(filename, O_WRONLY | O_CREAT, 0600);
+	if (ftruncate(fd, 0) == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
