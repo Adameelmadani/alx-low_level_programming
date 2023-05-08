4b96663a200dc7d0131301a9baee3036d9281d61
adam <adam@.gmail.com>
2023-05-08 16:49:59 -0700

    lddl

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <string.h>
+
 /**
   * create_file - create a file
   * @filename: filename
@@ -8,7 +10,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, a;
 
 	if (!filename)
 	{
@@ -27,6 +29,12 @@ int create_file(const char *filename, char *text_content)
 		close(fd);
 		return (-1);
 	}
+	a = write(fd, text_content, strlen(text_content));
 	close(fd);
+	if (a == -1)
+	{
+		write(1, "fail", 4);
+		return (-1);
+	}
 	return (1);
 }
