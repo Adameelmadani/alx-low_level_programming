4388dd81bcd4b5f1260b5a4625376adbd564599c
adam <adam@.gmail.com>
2023-05-08 16:10:34 -0700

    dldl

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,18 +18,26 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+	{
+		close(fd);
 		return (0);
+	}
 	string = malloc(sizeof(char) * letters);
 	if (!string)
+	{
+		close(fd);
 		return (0);
+	}
 	file_bytes = read(fd, string, letters);
 	if (file_bytes == -1)
 	{
+		close(fd);
 		free(string);
 		return (0);
 	}
 	byte_write = write(1, string, file_bytes);
 	free(string);
+	close(fd);
 	if (byte_write == -1)
 		return (0);
 	return (file_bytes);
