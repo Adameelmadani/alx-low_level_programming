91b4db344f542a777447bddaf3ee9365401c797e
adam <adam@.gmail.com>
2023-05-08 16:04:44 -0700

    dlld

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -28,7 +28,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		free(string);
 		return (0);
 	}
-	byte_write = write (1, string, file_bytes);
+	byte_write = write(1, string, file_bytes);
 	free(string);
 	if (byte_write == -1)
 		return (0);
