12f2e5b6e49e0b4ec9b4a1203e25c210f23ea730
Adameelmadani <125741857+Adameelmadani@users.noreply.github.com>
2023-05-25 20:39:00 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,21 +6,16 @@
   */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int size = strlen(b);
-	unsigned int i = 0, s = 1, l = 0, number = 0, n = 0, j = 0;
+	int j;
+	unsigned int numb = 0;
 
 	if (!b)
 		return (0);
-	for (i = 0; i < size ; i++)
+	for (j = 0; b[j]; j++)
 	{
-		s = 1;
-		if (b[size - 1 - i] != 49 && b[size - 1 - i] != 48)
+		if (b[j] < (0 + '0') || b[j] > (0 + '0'))
 			return (0);
-		for (j = 0; j < l; j++)
-			s = 2 * s;
-		n = (b[size - 1 - i] - '0') * s;
-		number = n + number;
-		l++;
+		numb = 2 * numb + (b[j] - '0');
 	}
-	return (number);
+	return (numb);
 }
