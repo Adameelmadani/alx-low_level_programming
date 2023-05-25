ee5a9800f57a59f1e54ffe431b7211ae768aef1f
Adameelmadani <125741857+Adameelmadani@users.noreply.github.com>
2023-05-25 21:56:16 +0100

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,60 +4,68 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-void closeit(int fd);
 /**
-  * main - main function
-  * @argc: argc
-  * @argv: argv
-  * Return: 1 or -1
-  */
-
-int main(int argc, char *argv[])
+ * error_file - checks if files can be opened.
+ * @fileFrom: file_from.
+ * @fileTo: file_to.
+ * @args: arguments vector.
+ * Return: no return.
+ */
+void handleFileError(int fileFrom, int fileTo, char *args[])
 {
-	char *buff[1024];
-	int fd, sfd;
-
-	if (argc != 3)
+	if (fileFrom == -1)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	if (access(argv[1], F_OK) != 0)
-		fd = -2;
-	else
-		fd = open(argv[1], O_RDONLY);
-	if (fd == -1 || fd == -2)
-	{
-		if (fd == -1)
-		{
-			if (close(fd) == -1)
-				closeit(fd);
-		}
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", args[1]);
 		exit(98);
 	}
-	sfd = open(argv[2], O_WRONLY | O_CREAT, 0664);
-	if (sfd == -1 || ftruncate(sfd, 0) == -1)
+	if (fileTo == -1)
 	{
-		if (close(sfd) == -1)
-			closeit(sfd);
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", args[2]);
 		exit(99);
 	}
-	read(fd, buff, 1024);
-	write(sfd, buff, 1024);
-	if (close(fd) == -1)
-		closeit(fd);
-	if (close(sfd) == -1)
-		closeit(sfd);
-	return (1);
 }
+
 /**
-  * closeit - close
-  * @fd: fd
-  */
-void closeit(int fd)
+ * main - check the code for Holberton School students.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
 {
-	dprintf(2, "Error: Can't close fd %d\n", fd);
-	exit(100);
+	int fileFrom, fileTo, errClose;
+	ssize_t bytesRead, bytesWritten;
+	char buffer[1024];
+	
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
+	fileFrom = open(argv[1], O_RDONLY);
+	fileTo = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	handleFileError(fileFrom, fileTo, argv);
+	bytesRead = 1024;
+	while (bytesRead == 1024)
+	{
+		bytesRead = read(fileFrom, buffer, 1024);
+		if (bytesRead == -1)
+			handleFileError(-1, 0, argv);
+		bytesWritten = write(fileTo, buffer, bytesRead);
+		if (bytesWritten == -1)
+			handleFileError(0, -1, argv);
+	}
+	errClose = close(fileFrom);
+	if (errClose == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileFrom);
+		exit(100);
+	}
+	errClose = close(fileTo);
+	if (errClose == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileTo);
+		exit(100);
+	}
+	return (0);
 }
