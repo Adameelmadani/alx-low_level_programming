9c3d7f2a0012b5c710ac22add40ea79c7557437e
adam <adam@.gmail.com>
2023-06-19 12:38:29 -0700

    dldl

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+  * add_dnodeint - add new node
+  * @head: head addres
+  * @n: int data
+  * Return: null or addres of new node
+  */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	if (head == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+	*head = new_node;
+	return (*head);
+}
